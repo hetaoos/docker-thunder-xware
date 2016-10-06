3e6202e88bcae1710390ce5f15a2f90c4e4c044d
Null <hetaoos@gmail.com>
2016-10-06 17:04:16 +0800

    finding & deleting linux core dump files.

--- start.sh
+++ start.sh
@@ -5,6 +5,7 @@ while true; do
     pid=`ps -ef|grep vod_httpserver|grep -v grep|awk '{print $2}'`
     if [ -z "$pid" ]; then
         ./bin/portal
+		find . -type f -regex ".*/core\.[0-9][0-9][0-9][0-9]$" -exec rm {} \;
     else
         sleep 60
     fi
